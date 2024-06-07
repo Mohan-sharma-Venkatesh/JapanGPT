e4da1c40fec4f074070c23b0180d3e6a94d37d80
Mohan-sharma-Venkatesh <mohan_venkatesh@heartcore.co.jp>
2024-06-07 12:42:07 +0900

    clean code

--- src/main/java/StartUpCalculator/ai/controller/Index.java
+++ src/main/java/StartUpCalculator/ai/controller/Index.java
@@ -1,25 +1,16 @@
 package StartUpCalculator.ai.controller;
+
+import StartUpCalculator.ai.dto.UserInput;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-class UserInput{
-  private String query;
-
-  public void setQuery(String query){
-    this.query= query;
-  }
-
-  public String getQuery(){
-    return this.query;
-  }
-}
-
 @Controller
 public class Index{
- 
+
   @RequestMapping(value="/")
   public String indexPage(){
     return "index.html";
---
+++ src/main/java/StartUpCalculator/ai/dto/UserInput.java
@@ -0,0 +1,13 @@
+package StartUpCalculator.ai.dto;
+
+public class UserInput{
+  private String query;
+
+  public void setQuery(String query){
+    this.query= query;
+  }
+
+  public String getQuery(){
+    return this.query;
+  }
+}
